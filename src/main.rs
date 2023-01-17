d120fdb3393f86cd401a37dbd4ae395b0d393a4d
Alexandria <alxpettit@gmail.com>
2023-01-16 18:25:03 -0700

    playing with assembly

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,52 @@
 #![feature(portable_simd)]
 use duration_human::DurationHuman;
+use itertools::Itertools;
 use std::hint::black_box;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 use std::time::Instant;
 use tinyvec::ArrayVec;
 
+// #[inline(never)]
+// fn mpsc_test_big_chunked_10x_with_iteration(
+//     tx: &Sender<[f32; 100]>,
+//     rx: &mut Receiver<[f32; 100]>,
+// ) {
+//     let array = [2f32; 100];
+//     for i in black_box(0..100) {
+//         let tx_send_data = array.iter().map(|v| black_box(black_box(*v) * 2.));
+//         let v: [f32; 100] = tx_send_data.collect_vec().try_into().unwrap();
+//         tx.send(black_box(v)).unwrap();
+//     }
+//     for i in black_box(0..100) {
+//         for s in black_box(rx.recv().unwrap()) {
+//             black_box(s);
+//         }
+//     }
+// }
+
+#[inline(never)]
+fn mpsc() {
+    let (tx, mut rx) = mpsc::channel::<f32>();
+    tx.send(black_box(5.)).unwrap();
+
+    black_box(rx.recv().unwrap());
+}
+
+#[inline(never)]
+fn five_plus_six() {
+    let a = black_box(5);
+    let b = black_box(6);
+    // Hardcodes 11 instead of doing calculation during runtime
+    black_box(5 + 6);
+}
+
+#[inline(never)]
 fn main() {
-    let mut a = 5;
-    a = 10;
-    println!("{}", a);
-    // black_box(a);
-    // let t = || {
-    //     println!("hi");
-    // };
-    // let v1 = Instant::now();
-    // // as closure: 27754ns
-    // // as closure: 17765ns
-    // // as closure: 17765ns
-    // //t();
-    //
-    // println!("hi");
-    // //println!("Hello, world!");
-    // // let a = ArrayVec<f32x8>::new();
-    // // dbg!("hi");
-    //
-    // let v1_elapsed = DurationHuman::from(v1.elapsed());
-    // println!("{}", v1_elapsed);
+    mpsc();
+    //let mut a = 5;
+
+    //let (tx, mut rx) = mpsc::channel::<[f32; 100]>();
+
+    //mpsc_test_big_chunked_10x_with_iteration(&tx, &mut rx);
 }
